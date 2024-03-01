494554656b1e044ea955dcf5bf9045505846b17c
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2024-03-01 14:48:06 -0500

    Update Brillia_app.py

--- Brillia_app.py
+++ Brillia_app.py
@@ -151,7 +151,7 @@ def process_ad_set_data(data, test, past_test_data, campaign):
 
     past_test_data = past_test_data[past_test_data['Test_Name'] == test]
     st.write(past_test_data)
-    ad_names = past_test_data.at[0, "Ad_Names"]
+    ad_names = past_test_data.iloc[0]["Ad_Names"]
     ad_names = ad_names.split(",")
           
     # Filter data on just ad_set
