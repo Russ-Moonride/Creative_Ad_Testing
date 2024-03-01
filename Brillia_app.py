e0fcdab44802d0e487c676a19e6d3d982b47bf49
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2024-03-01 14:10:25 -0500

    Update Brillia_app.py

--- Brillia_app.py
+++ Brillia_app.py
@@ -481,7 +481,7 @@ def main_dashboard():
             for test in past_tests:
                 st.write(test)
                 st.write(past_test_data['Test_Name'])
-                campaign = past_test_data[past_test_data['Test_Name' == test]].Campaign.iloc[0]
+                campaign = past_test_data[past_test_data['Test_Name'] == test].Campaign.iloc[0]
                 test_dfs[test] = process_ad_set_data(st.session_state.full_data, test, past_test_data, campaign)
           
             for test in test_dfs:
